06ed15ccfe0b83939675c76ae6e4094bd0330c0f
ko.chae <ko.chae93@gmail.com>
2021-08-26 12:26:33 +0900

    add habbit tracker

---
+++ src/components/habitAddForm.jsx
@@ -0,0 +1,24 @@
+import React, { PureComponent } from 'react';
+
+class HabitAddForm extends PureComponent {
+    inputRef = React.createRef();
+    
+    onSubmit = event => {
+        event.preventDefault();
+        const name = this.inputRef.current.value;
+        name && this.props.onAdd(name);
+        this.inputRef.current.value = "";
+    }
+
+    render() {
+        console.log('addform');
+        return (
+            <form className="add-form" onSubmit={this.onSubmit}>
+                <input ref={this.inputRef} type="text" className="add-input" placeholder="Habit"/>
+                <button className="add-button">Add</button>
+            </form>
+        );
+    }
+}
+
+export default HabitAddForm;
